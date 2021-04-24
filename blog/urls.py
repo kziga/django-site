851ba31dabc2e49f3d9ee70eda11eec840ae9784
kziga92 <kziga@purdue.edu>
2021-04-23 17:16:42 -0700

    Added css and post detail page

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	path('', views.post_list, name='post_list') # This means the main page should access the view post_list in views.py
+	path('', views.post_list, name='post_list'), # This means the main page should access the view post_list in views.py
+	path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
